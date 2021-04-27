3e671c11e91cd64a35472bd8508bcb0cca346fd7
Serhii Kovalov <serg3@Serhiis-MacBook-Pro.local>
2021-04-27 13:11:19 +0200

    adjustment

--- cmd/fibo/fibo.go
+++ cmd/fibo/fibo.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	nums := []int{1, 5, 8, 22}
+	nums := []int{1, 3, 5, 8, 20, 22}
 	for _, n := range nums {
 		if n > 20 {
 			fmt.Printf("Max number can be 20! You tried to reach %d number in sequence\n", n)
--- pkg/fibo/fibo.go
+++ pkg/fibo/fibo.go
@@ -3,7 +3,7 @@ package fibo
 // Seq returns a Fibonacci's sequense
 func Seq(n int) int {
 	x0, x1 := 0, 1
-	for i := 0; i <= n; i++ {
+	for i := 1; i <= n; i++ {
 		x0, x1 = x1, x0+x1
 	}
 	return x1
